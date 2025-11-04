b51ddd42b59cc179166814edf2d9aa3ca6cf4aef
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-04 10:55:08 +0100

    Update messages.go

--- i18n/fr/LC_MESSAGES/messages.go
+++ i18n/fr/LC_MESSAGES/messages.go
@@ -1,2 +1,23 @@
-msgid "Welcome to the advanced Kubuverse page in fr"
-msgstr "Bienvenue sur la page avancée de Kubuverse !"
+package LC_MESSAGES
+
+import "github.com/nicksnyder/go-i18n/v2/i18n"
+
+// French translations
+var Messages = []*i18n.Message{
+	{
+		ID:    "HelloWorld",
+		Other: "Bonjour le monde",
+	},
+	{
+		ID:    "WelcomeUser",
+		Other: "Bienvenue, {{.Name}}!",
+	},
+	{
+		ID:    "ErrorNotFound",
+		Other: "Erreur : élément introuvable.",
+	},
+	{
+		ID:    "SubmitSuccess",
+		Other: "Votre soumission a été envoyée avec succès.",
+	},
+}
