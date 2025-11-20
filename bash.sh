7f848ab5ad20f6727ec0d70539223a534ae1c792
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-20 14:36:30 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,12 @@
+```
+$ git clone git://github.com/lxc/lxcfs
+cd lxcfs
+./bootstrap.sh
+./configure
+make
+sudo mkdir -p /var/lib/lxcfs
+sudo ./lxcfs -s -f -o allow_other /var/lib/lxcfs/
+
 gh extension install github/gh-actions-importer
 kubectl apply -f deploy/kubuverse-deployment.yaml
 
