d9265046043463cb6abf9acf7068d66eb3dd11e5
Seriki yakub <sandraisom00@gmail.com>
2025-12-23 16:52:33 +0100

    Update and rename _page.rexx to _page.jsx

--- _page.jsx
+++ _page.jsx
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
-
+ 
 import { dev } '659c5f7545bbb9ad8cfc423f?pageType=blueprint&graphType=software_design'
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
