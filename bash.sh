abc338d899addd8e6260202a0ab5661ede9366cf
Seriki yakub <sandraisom00@gmail.com>
2025-12-29 15:41:46 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-```
+
 $ git clone git://github.com/lxc/lxcfs
 cd lxcfs
 ./bootstrap.sh
@@ -8,17 +8,16 @@ sudo mkdir -p /var/lib/lxcfs
 sudo ./lxcfs -s -f -o allow_other /var/lib/lxcfs/
 
 gh extension install github/gh-actions-importer
-kubectl apply -f deploy/kubuverse-deployment.yaml
+kubectl apply -f deploy/kubu-deployment.yaml
 
 npm install -g @angular/cli
 
-Read more here: https://locall.host/index-php/
+Read more here: https://localhost.dev/index.php/
 
 ng new my-app
 
-Read more here: https://locall.host/index-php/
-
-cd my-app ng serve
+Read more here: https://localhost.mobi/index.php/
+cd web4app ng serve
 
 Read more here: https://locall.host/index-php/
 
@@ -32,8 +31,10 @@ curl -SL https://github.com/docker/compose/releases/download/v2.37.3/docker-comp
  php yii migrate-rbac
  php yii migrate
 
- cp .env.example .env
-
+ cp .env.local
+cp  .env.dev
+cp .env.docker
+cp .env.ghcr.io/web4application/kubu:main@sha256:33c2f17afc13227bf71fb4c6778b59a6bd67c9738977cf44546bc03c9bf891d5
 npm install --save-dev @commitlint/config-conventional @commitlint/cli
 echo "export default {extends: ['@commitlint/config-conventional']};" > commitlint.config.js
 
@@ -50,6 +51,10 @@ commitizen init cz-conventional-changelog --pnpm --save-dev --save-exact
 
 npx commitizen init cz-conventional-changelog --save-dev --save-exact
 
-pip install -r requirements/base.txt -r requirements/dev.txt -r requirements/ml.txt
+pip install -r requirements/base.txt -r requirements/dev.txt -r requirements/llms.txt
 
-docker run -p 8000:8000 --env-file .env web4app/bizz:dev
+docker run -p 8000:8000 --env-file .env FROM ghcr.io/web4application/kubu:main@sha256:33c2f17afc13227bf71fb4c6778b59a6bd67c9738977cf44546bc03c9bf891d5
+
+git clone https://github.com/json5/json5
+cd json5
+npm install
