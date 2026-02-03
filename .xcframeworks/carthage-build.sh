b52d36c73976ae33960ea54cd5049ce5e53331d2
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-04 00:19:06 +0100

    Create carthage-build.sh

---
+++ .xcframeworks/carthage-build.sh
@@ -0,0 +1,6 @@
+if [ -f "Cartfile.resolved" ]; then
+  # Only runs bootstrap if dependencies are missing or out of date
+  /usr/local/bin/carthage bootstrap --platform iOS --use-xcframeworks --cache-builds
+else
+  echo "No Cartfile.resolved found. Skipping Carthage build."
+fi
