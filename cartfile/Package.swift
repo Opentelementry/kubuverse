befa465e8355afa01d7448726a0cfbfce49d2fee
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-03 22:46:33 +0100

    Create Package.swift

---
+++ cartfile/Package.swift
@@ -0,0 +1,5 @@
+github "AFNetworking/AFNetworking" ~> 4.0
+  dependencies: [
+    .package(url: "https://github.com/AFNetworking/AFNetworking.git", .upToNextMajor(from: "4.0.0"))
+]
+#import "AFNetworking.h"
