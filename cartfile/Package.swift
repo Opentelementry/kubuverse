c6d5b5ed8b61fb33ab1d8666201f0f1c721f863b
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-03 22:51:55 +0100

    Update Package.swift

--- cartfile/Package.swift
+++ cartfile/Package.swift
@@ -3,3 +3,6 @@ github "AFNetworking/AFNetworking" ~> 4.0
     .package(url: "https://github.com/AFNetworking/AFNetworking.git", .upToNextMajor(from: "4.0.0"))
 ]
 #import "AFNetworking.h"
+dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1"))
+]
